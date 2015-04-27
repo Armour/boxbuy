9cdf54d6e3a3852f4780e487c4b738038fba9335
Armour <497052684@qq.com>
2015-04-28 00:10:22 +0800

    finished login page's draft

--- YunGeZi/ViewController.h
+++ YunGeZi/ViewController.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *textUsername;
+@property (weak, nonatomic) IBOutlet UITextField *textPassword;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
--- YunGeZi/ViewController.m
+++ YunGeZi/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
@@ -14,14 +15,94 @@
 
 @implementation ViewController
 
+enum {
+    textUsernameTag = 0,
+    textPasswordTag
+};
+
+- (void)prepareMyTextField {
+    self.textUsername.delegate = self;
+    self.textPassword.delegate = self;
+    self.textUsername.tag = textUsernameTag;
+    self.textPassword.tag = textPasswordTag;
+    self.textUsername.layer.cornerRadius = 10.0f;
+    self.textPassword.layer.cornerRadius = 10.0f;
+    //self.textUsername.layer.masksToBounds = YES;
+    //self.textPassword.layer.masksToBounds = YES;
+    self.textUsername.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    self.textPassword.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+    self.textUsername.layer.borderWidth = 1.0f;
+    self.textPassword.layer.borderWidth = 1.0f;
+    self.textUsername.clearButtonMode = UITextFieldViewModeWhileEditing;
+    self.textPassword.clearButtonMode = UITextFieldViewModeWhileEditing;
+}
+
+- (void)prepareMyButton {
+    [self.loginButton setBackgroundColor:[UIColor purpleColor]];
+    self.loginButton.layer.cornerRadius = 10.0f;
+}
+
+- (IBAction)loginButtonTouchUpInside:(UIButton *)sender {
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self prepareMyTextField];
+    [self prepareMyButton];
+
+    /*
+    NSHTTPCookie *cookie;
+    NSHTTPCookieStorage *cookieJar = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    for (cookie in [cookieJar cookies]) {
+        NSLog(@"%@", cookie);
+    }
+    */
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    switch (textField.tag) {
+        case textUsernameTag:
+            self.textUsername.layer.borderColor = [[UIColor colorWithRed:0 green:204 blue:100 alpha:1] CGColor];
+            break;
+        case textPasswordTag:
+            self.textPassword.layer.borderColor = [[UIColor colorWithRed:0 green:204 blue:100 alpha:1] CGColor];
+            break;
+        default:
+            break;
+    }
+    return YES;
+}
+
+-(BOOL)textFieldShouldEndEditing:(UITextField *)textField {
+    switch (textField.tag) {
+        case textUsernameTag:
+            self.textUsername.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+            break;
+        case textPasswordTag:
+            self.textPassword.layer.borderColor = [[UIColor lightGrayColor] CGColor];
+            break;
+
+        default:
+            break;
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (IBAction)backgroundTap:(id)sender {
+    [self.view endEditing:YES];
 }
 
 @end
