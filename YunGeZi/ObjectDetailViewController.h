bfe8b92f9b0c76e2a3658479a1bc0b1d24a60c06
Armour <497052684@qq.com>
2015-08-31 18:29:45 -0700

    add delete button when self is seller

--- YunGeZi/ObjectDetailViewController.h
+++ YunGeZi/ObjectDetailViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ObjectDetailViewController : UIViewController <UIScrollViewDelegate>
+@interface ObjectDetailViewController : UIViewController <UIScrollViewDelegate, UIAlertViewDelegate>
 
 @property (strong, nonatomic) NSString *objectNumber;
 
